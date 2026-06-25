4ab0a77c8f526a04da10f9f7c03cb8ab797e5d58
Dario Žepić <dzepic@student.unipu.hr>
2026-06-25 18:56:31 +0200

    dodavanje naslova na homepage-u

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@ const authStore = useAuthStore()
       <div class="absolute inset-0 bg-linear-to-r from-[#061222]/95 via-[#061222]/65 to-transparent z-1"></div>
 
       <div class="relative z-10 max-w-155 px-5">
-        // dodavanje naslova, podnaslova i opisa
+        <!-- dodavanje naslova, podnaslova i opisa -->
         <p class="text-[#4facfe] text-xs font-semibold tracking-widest mb-4">CAR COMPARISON PLATFORM</p>
         
         <h1 class="text-5xl font-medium leading-tight tracking-tight mb-6">Clear and Simple<br />Car Compare.</h1>
@@ -24,7 +24,7 @@ const authStore = useAuthStore()
           Navigate and compare technical specifications of various vehicles in one place. Get accurate and objective data to help you find the perfect car.
         </p>
         <div class="flex gap-4">
-          // gumb koji vodi na compare 
+          <!-- gumb koji vodi na compare --> 
           <router-link 
             to="/compare" 
             class="px-7 py-3.5 text-sm font-medium bg-blue-600 hover:bg-blue-700 rounded-md transition-colors flex items-center gap-2 group"
