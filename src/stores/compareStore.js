93be9c8d9c37432ffcba0bbdf78b0c97214fc007
Dario Žepić <dzepic@student.unipu.hr>
2026-07-19 18:59:12 +0200

    compareStore koji će složiti kako bi se auti sa browse-a dodali na compare

---
+++ src/stores/compareStore.js
@@ -0,0 +1,26 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCompareStore = defineStore('compare', () => {
+  // Držimo fiksno 4 slota, inicijalno prazni (null)
+  const selectedCars = ref([null, null, null, null])
+
+  // Funkcija traži prvi slobodan slot u compareu i dodaje auto u njega
+  const addCarToCompare = (car) => {
+    const freeSlotIndex = selectedCars.value.findIndex(slot => slot === null)
+    
+    if (freeSlotIndex !== -1) {
+      selectedCars.value[freeSlotIndex] = car
+      return true // Uspješno dodano
+    } else {
+      alert('Svi slotovi za usporedbu su popunjeni!')
+      return false
+    }
+  }
+
+  const clearSlot = (index) => {
+    selectedCars.value[index] = null
+  }
+
+  return { selectedCars, addCarToCompare, clearSlot }
+})
